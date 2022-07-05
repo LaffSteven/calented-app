e973fd0a78f775c1be12ec49f49041f0e9db2dd2
LaffSteven <steve.laff@gmail.com>
2022-07-05 10:23:13 -0400

    set up CRUD routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+import { response } from 'express';
+
+
 
 function App() {
+
+  const [calEventList, setCalEventList] = useState([]);
+
+  const getCalEventList = () => {
+    axios.get('https://calented-server.herokuapp.com/api/calevents')
+      .then((response) => {
+        setCalEventList(response.data)
+      })
+  }
+  const handleCreate = (newCalEvent) => {
+    axios.post('https://calented-server.herokuapp.com/api/calevents', newCalEvent)
+      .then((response) => {
+        setCalEventList([...calEventList, response.data])
+      })
+  }
+  const handleUpdate = (updatedCalEvent) => {
+    axios.put('https://calented-server.herokuapp.com/api/calevents/' + updatedCalEvent.id, updatedCalEvent)
+      .then((response) => {
+        setCalEventList(calEventList.map(calEvent) => {
+          return calEvent.id !== response.data.id ? calEvent : response.data
+        })
+      })
+  }
+  const handleDelete = (deletedCalEvent) => {
+    axios.delete('https://calented-server.herokuapp.com/api/calevents/' + deletedCalEvent.id)
+      .then((response) => {
+        setCalEventList(calEventList.filter(calEvent => calEvent.id !== deletedCalEvent.id))
+      })
+  }
+
+  useEffect(() => {
+    getCalEventList();
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Calented</h1>
+      <ul>
+        {calEventList.map((calEvent) => {
+          return <li>{calEvent.title}: </li>
+        })}
+      </ul>
     </div>
   );
 }
